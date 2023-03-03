3c5ecb34e87608b6d68b5f1580b34b60690269b4
nawm77 <alpha050603@gmail.com>
2023-03-03 17:53:57 +0300

    Initial commit

---
+++ src/main/java/com/example/miitnavigation/MiitNavigationApplication.java
@@ -0,0 +1,13 @@
+package com.example.miitnavigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiitNavigationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiitNavigationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miitnavigation/MiitNavigationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miitnavigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiitNavigationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
